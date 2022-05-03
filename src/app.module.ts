8ce5e54c1ff07cb2dc8f6a96e414689981feecab
Batlbot1 <vlad.nesterov.982@gmail.com>
2022-05-03 13:47:39 +0300

    Create and setting docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        envFilePath: ".env"
+      }),
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    port: 5432,
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRESS_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
